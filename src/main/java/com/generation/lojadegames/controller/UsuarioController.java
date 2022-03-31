e7b45c9a97c6aab619b8414d61cb97ebc5941ddd
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-03-31 11:59:28 -0300

    Alterações na controller

--- src/main/java/com/generation/lojadegames/controller/UsuarioController.java
+++ src/main/java/com/generation/lojadegames/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,16 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> putUsuario (@Valid @RequestBody Usuario usuario) {
 		return usuarioRepository.findById(usuario.getId())
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteTema (@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> {
+					usuarioRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
 				.orElse(ResponseEntity.notFound().build());
 	}
 }
